501527e98c535664f3f0bb9a144bc835a4cfba9d
Faizan Haider <hfaizan53@gmail.com>
2022-06-07 00:26:54 +0500

    linking set

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#start'
   get '/terms-and-conditions', to: 'pages#terms_and_conditions'
-  get '/home-privacy', to: 'pages#home_privacy_policy'
+  get '/your-privacy-rights', to: 'pages#home_privacy_policy'
   get '/cookie-policy', to: 'pages#home_cookie_policy'
   # get '/start', to: 'pages#start'
 end
